457fc30fc61c5b5ee561a43ab097a87791db734f
Dan6erbond <moravrav@gmail.com>
2020-07-11 20:23:11 +0200

    Add material and form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,22 @@ import { HomeComponent } from './home/home.component';
 import { AnimalsComponent } from './animals/animals.component';
 import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 import { ErrorComponent } from './error/error.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BasicFormComponent } from './basic-form/basic-form.component';
+import { NestedFormComponent } from './nested-form/nested-form.component';
+import { ArrayFormComponent } from './array-form/array-form.component';
+import { ValidFormComponent } from './valid-form/valid-form.component';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AnimalsComponent, AnimalDetailComponent, ErrorComponent],
+  declarations: [AppComponent, HomeComponent, AnimalsComponent, AnimalDetailComponent, ErrorComponent, BasicFormComponent, NestedFormComponent, ArrayFormComponent, ValidFormComponent, SubmitFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
